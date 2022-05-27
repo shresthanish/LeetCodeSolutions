6124584dcdf3a6a873eb3616c847da22a4a0daf8
Anish Shrestha <100459851+shresthanish@users.noreply.github.com>
2022-05-26 21:27:59 -0400

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] sum = new int[2];
+        int i,j;
+        for(i=0; i<nums.length;i++){
+        for(j = i+1; j<nums.length; j++){
+            if(nums[i]+nums[j] == target){
+                sum[0] = i;
+                sum[1] = j;
+                return sum;
+            }
+        }
+    }
+        return sum;
+    }
+}
